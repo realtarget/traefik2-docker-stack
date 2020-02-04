06d7fb999941e0673b7d395305f815a00e08e5b5
Davin Shearer <scholarsmate@gmail.com>
2020-02-04 13:32:32 -0500

    initial setup script

---
+++ setup.sh
@@ -0,0 +1,2 @@
+docker-compose -f atlassian/docker-compose.yml -f gitlab/docker-compose.yml -f nexus/docker-compose.yml -f portainer/docker-compose.yml -f rocketchat/docker-compose.yml -f traefik2/docker-compose.yml up -d
+
