787a017b80930cfb18aaaa3a709bf9a4fe00d5a9
Davin Shearer <davin.shearer@evc.ctc.com>
2020-02-04 17:51:03 -0500

    add setup script and fix docker compose version requirements to version 3

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker network create traefik-proxy
+docker-compose -f atlassian/docker-compose.yml -f gitlab/docker-compose.yml -f nexus/docker-compose.yml -f portainer/docker-compose.yml -f rocketchat/docker-compose.yml -f traefik2/docker-compose.yml up -d
+
