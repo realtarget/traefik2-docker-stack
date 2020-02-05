d53ab03499f91d9c94f25626235eed06886ac6a0
Davin Shearer <davin.shearer@evc.ctc.com>
2020-02-04 19:06:37 -0500

    search for docker-compose in /usr/local/bin as well

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 set -ex
 
 docker network create traefik-proxy
-docker-compose -f atlassian/docker-compose.yml -f gitlab/docker-compose.yml -f nexus/docker-compose.yml -f portainer/docker-compose.yml -f rocketchat/docker-compose.yml -f traefik2/docker-compose.yml up -d
+PATH="$PATH":/usr/local/bin docker-compose -f atlassian/docker-compose.yml -f gitlab/docker-compose.yml -f nexus/docker-compose.yml -f portainer/docker-compose.yml -f rocketchat/docker-compose.yml -f traefik2/docker-compose.yml up -d
 
